becabcd2b9a64821b6d50a56628ad1703dce7818
Sude Guler <sudegulerrss@gmail.com>
2025-07-28 13:43:46 +0300

    Inventory Database
    Now, thanks to the item ID registered in the database, items in the player's inventory can be changed simply by changing the ID.

--- Assets/ScriptableObjects/Inventory/Scripts/InventoryObject.cs
+++ Assets/ScriptableObjects/Inventory/Scripts/InventoryObject.cs
@@ -4,39 +4,50 @@ using System.IO.Enumeration;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")] 
-public class InventoryObject : ScriptableObject
+public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
+
 
 {
+    public ItemDatabaseObject database;
     public List<InventorySlot> Container = new List<InventorySlot>();
     public void AddItem(ItemObject _item, int _amount)
     {
-        // First, we need to check whether the item is in stock
-
-        bool hasItem = false;
+        
         for (int i = 0; i < Container.Count; i++)
         {
             if (Container[i].item == _item)
             {
                 Container[i].AddAmount(_amount);
-                hasItem = true;
-                break;
+                return;
             }
         }
-        if (!hasItem)
-        {
-            Container.Add(new InventorySlot(_item, _amount));
-        }
+        Container.Add(new InventorySlot(database.GetId[_item], _item, _amount));
+        
+    }
 
+    public void OnAfterDeserialize()
+    {
+        for (int i = 0;i < Container.Count;i++)
+        {
+            Container[i].item = database.GetItem[Container[i].ID];
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+        
     }
 }
 
 [System.Serializable] // when we add it to a system within Unity, it will be serialized
 public class InventorySlot
 {
+    public int ID;
     public ItemObject item; // general object stored in the inventory item
     public int amount; // total quantity of general objects stored in the inventory item
-    public InventorySlot(ItemObject _item, int _amount)
+    public InventorySlot(int _id,  ItemObject _item, int _amount)
     {
+        ID = _id;
         item = _item;
         amount = _amount;
     }
