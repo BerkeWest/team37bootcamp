e808422ef5036fe5022fc7ad0896d815149f882e
Berke Batı <berkebati@gmail.com>
2025-07-30 00:40:41 +0300

    map updates

--- Assets/Scripts/MapLoad.cs
+++ Assets/Scripts/MapLoad.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class MapLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform spawnPoint;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = spawnPoint.position;
+            other.transform.rotation = spawnPoint.rotation;
+        }
     }
 }
