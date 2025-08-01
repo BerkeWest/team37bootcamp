023307933c72e43b1b2656abb6917ab9ec256ee8
Berke Batı <berkebati@gmail.com>
2025-08-01 20:16:55 +0300

    inventory images and enemy spawner

--- Assets/Scripts/MapLoad.cs
+++ Assets/Scripts/MapLoad.cs
@@ -6,11 +6,32 @@ public class MapLoad : MonoBehaviour
 {
     [SerializeField] private Transform spawnPoint;
     
+    public GameObject enemyPrefab;             
+    public Transform[] spawnPoints;           
+    public int numberOfEnemiesToSpawn = 5;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerControllerS>().GoToPosition(spawnPoint);
+            SpawnEnemies();
+        }
+    }
+    
+    private void SpawnEnemies()
+    {
+        System.Collections.Generic.List<int> availableIndices = new System.Collections.Generic.List<int>();
+        for (int i = 0; i < spawnPoints.Length; i++)
+            availableIndices.Add(i);
+        
+        for (int i = 0; i < numberOfEnemiesToSpawn && availableIndices.Count > 0; i++)
+        {
+            int randomIndex = Random.Range(0, availableIndices.Count);
+            int spawnIndex = availableIndices[randomIndex];
+            availableIndices.RemoveAt(randomIndex);
+
+            Instantiate(enemyPrefab, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
         }
     }
 }
