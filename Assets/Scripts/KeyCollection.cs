d5e71f0c669065fbdf8651eefd4dd1e6f5285b7c
Berke Batı <berkebati@gmail.com>
2025-08-02 20:23:43 +0300

    trapdoor logic change, key collection, portal and merchant assets added

---
+++ Assets/Scripts/KeyCollection.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class KeyCollection : MonoBehaviour
+{
+    [SerializeField] private GameObject key;
+    [SerializeField] private GameObject door;
+    [SerializeField] private float openAngle = 90f;
+    private bool hasKey = false;
+    private bool doorOpened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == key)
+        {
+            hasKey = true;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject == door && hasKey && !doorOpened)
+        {
+            Vector3 currentRotation = door.transform.eulerAngles;
+            door.transform.eulerAngles = new Vector3(currentRotation.x, openAngle, currentRotation.z);
+
+            doorOpened = true;
+            hasKey = false;
+        }
+    }
+}
+
